805388eb7fc2d86360372c08ec146203b91a01c4
ghazal <helmzadeh.ghazal@gmail.com>
2017-08-12 04:52:42 +0430

    Add project files.

---
+++ App1/App1/Page2.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page2 : ContentPage
+	{
+		public Page2 ()
+		{
+			InitializeComponent ();
+		}
+	    private async void NavigateButton_OnClicked(object sender, EventArgs e)
+	    {
+	        await Navigation.PushAsync(new MainPage());
+	    }
+    }
+}
